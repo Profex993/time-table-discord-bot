8be3490edd2fdad5a5705bc2c9a5808aa36fe1ce
Profex993 <68472681+Profex993@users.noreply.github.com>
2025-04-13 19:01:24 +0200

    day format bug fixed, resend command added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const {Client, Events, GatewayIntentBits} = require("discord.js");
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
 const config = require("./config.json");
 
 const xlsx = require("xlsx");
 const fs = require("fs");
-const { SPPull } = require("sppull");
+const {SPPull} = require("sppull");
 
 let searchedClasses = config.forClasses.split(" ");
 let classMap = new Map();
@@ -27,13 +29,38 @@ const sppullOptions = {
 
 client.login(config.discordToken);
 
-client.on(Events.ClientReady, client => {
+function download() {
+    SPPull.download(sppullContext, sppullOptions).then(function () {
+        fs.rename("./data/" + config.siteFileName, "./data/data.xlsx", () => {
+            processXLSX();
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+client.once(Events.ClientReady, client => {
     let channelIDs = config.chatId.split(" ");
     for (let i = 0; i < searchedClasses.length; i++) {
         classMap.set(searchedClasses[i], client.channels.cache.get(channelIDs[i]));
     }
 
+    download();
     start();
+
+    client.on(Events.MessageCreate, async message => {
+        if (message.author === config.ownerId) {
+
+            let words = message.content.match(/\b(\w+)\b/g);
+            if (!words) words = [];
+            words = words.map(w => w.toLowerCase());
+
+            if (words.includes("bot_resend")) {
+                download();
+            }
+        }
+    });
+
 });
 
 function start() {
@@ -78,8 +105,9 @@ function run() {
         setInterval(function () {
             f1();
         }, 86400000);
-       
+
     }
+
     function f1() {
         let date2 = new Date();
         let day = date2.getDay();
@@ -89,31 +117,25 @@ function run() {
     }
 }
 
-function download() {
-    SPPull.download(sppullContext, sppullOptions).then(function () {
-        fs.rename("./data/" + config.siteFileName, "./data/data.xlsx", () => {
-            processXLSX();
-        });
-    }).catch(function (err) {
-        console.log(err);
-    });
-}
-
 function processXLSX() {
     let xlsxData, wb, ws;
-    let date = new Date();
+
 
     wb = xlsx.readFile("./data/data.xlsx");
 
-    let dateFormat = (date.getDate() + 1) + "" + (date.getMonth() + 1) + "" + date.getFullYear();
+    let date = new Date();
+    date.setDate(date.getDate() + 1);
+    let dateFormat = (date.getDate()) + "" + (date.getMonth() + 1) + "" + date.getFullYear();
+
     for (let i = wb.SheetNames.length - 1; i > 0; i--) {
-        if (wb.SheetNames[i].replace(/[^0-9]/g, '') === dateFormat) {
+        if (wb.SheetNames[i].replace(/[^0-9]/g, '') === dateFormat ||
+            wb.SheetNames[i].replace(/[^0-9]/g, '') === 0 + "" + dateFormat) {
             ws = wb.Sheets[wb.SheetNames[i]];
             break;
         }
     }
 
-    xlsx.utils.sheet_add_aoa(ws, [["class", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10"]], { origin: "A1" });
+    xlsx.utils.sheet_add_aoa(ws, [["class", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10"]], {origin: "A1"});
     xlsxData = xlsx.utils.sheet_to_json(ws);
 
     for (let i = 0; i < xlsxData.length; i++) {
@@ -129,7 +151,7 @@ function processXLSX() {
                     }
 
                     if (out !== "") {
-                        classMap.get(currentClass).send("Změny rozvrhu pro třídu " + currentClass + " na den: " + (date.getDate() + 1) + "." + (date.getMonth() + 1) + ".\n" + out);
+                        classMap.get(currentClass).send("ZmÄ›ny rozvrhu pro tĹ™Ă­du " + currentClass + " na den: " + (date.getDate() + 1) + "." + (date.getMonth() + 1) + ".\n" + out);
                     }
                 }
             }
